44c1de45b99f43270609a0f0849830fa19c9c3a5
Imre Toth <i.toth@vidaxl.com>
2017-07-19 19:40:28 +0200

    more generic printing

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ module.exports = function (parameters) {
     parameters = parameters || {};
 
     let defaultParameters = {
-        alternateParameterPrint: true
+        alternateParameterPrint: true,
+        // face: 'default'
+        face: 'flaming-sheep'
     };
 
     let calculatedParamteres =  merge(parameters, defaultParameters);
@@ -37,30 +39,42 @@ module.exports = function (parameters) {
             return msg;
         },
 
-        log: function () {
-            let msg = '';
-            for (let i = 0; i < arguments.length; i++) {
-                let newMsg = '';
-                let value = arguments[i];
-                let head = '\n--- Beginnig Parameter ' + i + '. ---\n'
-                newMsg += head;
-                let stringifyedParameter = stringifyObject(value, {
-                    indent: '  ',
-                    singleQuotes: false
-                });
-                newMsg += cowlog._printMsg(i, stringifyedParameter);
-                let foot = '\n--- End Parameter ' + i + '. ---\n';
-                newMsg += foot;
-                msg += newMsg;
-            };
+        _makeLogger:function(argumentsFrom) {
+            return function () {
+                let msg = '';
+                for (let i = argumentsFrom; i < arguments.length; i++) {
+                    let newMsg = '';
+                    let value = arguments[i];
+                    let head = '\n--- Beginnig Parameter ' + i + '. ---\n'
+                    newMsg += head;
+                    let stringifyedParameter = stringifyObject(value, {
+                        indent: '  ',
+                        singleQuotes: false
+                    });
+                    newMsg += cowlog._printMsg(i, stringifyedParameter);
+                    let foot = '\n--- End Parameter ' + i + '. ---\n';
+                    newMsg += foot;
+                    msg += newMsg;
+                }
+                ;
 
-            console.log(cowsay.say({
-                text: msg,
-                e: "oO",
-                T: "U "
-            }));
+                console.log(cowsay.say({
+                    text: msg,
+                    e: "oO",
+                    T: "U ",
+                    f: calculatedParamteres.face
+                }));
+            }
+        },
+
+        log: function(){
+            cowlog._makeLogger(0).apply(this,arguments)
         }
     };
+
+    // logFunction: function (function) {
+    //
+    // }
 
     return cowlog;
 };
