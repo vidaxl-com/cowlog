7f5df37565a9b8a213c80d5bef7237de254027ad
Imre Toth <i.toth@vidaxl.com>
2017-08-17 13:44:25 +0000

    Added back more cartoons

--- configs/default.js
+++ configs/default.js
@@ -4,7 +4,12 @@ module.exports = {
     alternateParameterPrint: true,
         face: function () {
             let faces = [
-                'stegosaurus',
+                'beavis.zen','bong','bud-frogs','bunny','cheese','cower','daemon','default','doge','dragon',
+                'dragon-and-cow','elephant','elephant-in-snake','eyes','flaming-sheep','ghostbusters','goat',
+                /*'head-in'*/,'hedgehog','hellokitty',/*'kiss'*/,'kitty','koala','kosh','luke-koala','mech-and-cow','meow',
+                'milk','moofasa','moose',/*'mutilated'*/,'ren','satanic','sheep','skeleton','small',/*'sodomized'*/,
+                'squirrel','stegosaurus','stimpy','supermilker','surgery','telebears','turkey','turtle','tux',
+                'vader','vader-koala','whale','www',
                 ];
             return faces[Math.floor(Math.random()*faces.length)];
         }(),
