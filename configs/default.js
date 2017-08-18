7ab3c3fa73d48140e70782f0960cdfdfc7b20f46
Imre Toth <i.toth@vidaxl.com>
2017-08-18 19:10:54 +0000

    New fancy stuff is added

--- configs/default.js
+++ configs/default.js
@@ -1,21 +1,23 @@
 const cowsay = require('cowsay');
 module.exports = {
     preset: true,
-    alternateParameterPrint: true,
-        face: function () {
-            let faces = [
-                'beavis.zen','bong','bud-frogs','bunny','cheese','cower','daemon','default','doge','dragon',
-                'dragon-and-cow','elephant','elephant-in-snake','eyes','flaming-sheep','ghostbusters','goat',
-                /*'head-in'*/,'hedgehog','hellokitty',/*'kiss'*/,'kitty','koala','kosh','luke-koala','mech-and-cow','meow',
-                'milk','moofasa','moose',/*'mutilated'*/,'ren','satanic','sheep','skeleton','small',/*'sodomized'*/,
-                'squirrel','stegosaurus','stimpy','supermilker','surgery',/*'telebears'*/,'turkey','turtle','tux',
-                'vader','vader-koala','whale','www',
-                ];
-            return faces[Math.floor(Math.random()*faces.length)];
-        }(),
+    alternateParameterPrint: false,
+    alternateParameterHeadPrint: true,
+    alternateParameterFootPrint: true,
+    face: function () {
+        let faces = [
+            'beavis.zen','bong','bud-frogs','bunny','cheese','cower','daemon','default','doge','dragon',
+            'dragon-and-cow','elephant','elephant-in-snake','eyes','flaming-sheep','ghostbusters','goat',
+            /*'head-in'*/,'hedgehog','hellokitty',/*'kiss'*/,'kitty','koala','kosh','luke-koala','mech-and-cow','meow',
+            'milk','moofasa','moose',/*'mutilated'*/,'ren','satanic','sheep','skeleton','small',/*'sodomized'*/,
+            'squirrel','stegosaurus','stimpy','supermilker','surgery',/*'telebears'*/,'turkey','turtle','tux',
+            'vader','vader-koala','whale','www',
+        ];
+        return faces[Math.floor(Math.random()*faces.length)];
+    }(),
     activity:function(){
-    let activities = [cowsay.say,cowsay.think];
-    return activities[Math.floor(Math.random()*activities.length)];
+        let activities = [cowsay.say,cowsay.think];
+        return activities[Math.floor(Math.random()*activities.length)];
     }(),
     registerGlobal:true
 };
--- index.js
+++ index.js
@@ -10,102 +10,231 @@ module.exports = function (parameters) {
     const calculatedParamteres = require('./app/configParser')(parameters);
     const stackTrace = require('stacktrace-js');
     const randomstring = require("randomstring");
-    const s = require('shelljs');
-    const fs = require('fs');
+    // const s = require('shelljs');
+    var fs = require('fs');
     const os = require('os');
-    const tmpDir = os.tmpdir();
     const path = require('path');
+    const fsPath = require('fs-path');
+    const sha256 = require('sha256');
 
+    const removeNumberOfEntitiesSelfReferncesFromStacktrace = 2;
+    const hrTime = process.hrtime();
+
+    const tmpDir = os.tmpdir();
+    const cowlogTmpDir = path.join(tmpDir,'cowlog/');
+    const delimiterInFiles = '\n\n--------------------------------------------------\n--------------------------------------------------\n';
+
+    const makeLogFile = function (string, logTypeString) {
+
+        const _insertToString= function(str, index, value) {
+            return str.substr(0, index) + value + str.substr(index);
+        };
+
+        const _makeFileNameHashPath = function (string) {
+            return _insertToString(sha256(string),2,'/')
+        };
+
+        const _makeHashPath = function(relativeFilePath, logTypeString){
+            if(!logTypeString){
+                logTypeString = '.log';
+            }
+            return path.join(cowlogTmpDir + relativeFilePath) + logTypeString;
+        };
+
+        const _makeHash = function(string){
+            return sha256(string)
+        };
+
+        let hash = _makeHash(string);
+        let relativeFilePath = _makeFileNameHashPath(hash);
+        let filePath = _makeHashPath(relativeFilePath, '_' + logTypeString);
+
+        if (!fs.existsSync(filePath)) {
+            fsPath.writeFileSync(filePath, string)
+        }
+
+        return filePath;
+    };
 
     let cowlog = {
 
-        _printMsg : function (iterator, message) {
-            let msg = '';
+        _fileLogs: {},
 
-            if (calculatedParamteres.alternateParameterPrint){
-                let isInverseColor = isEven(iterator);
+        _collectedLogs: [],
 
-                if (isInverseColor) {
-                    msg += message.inverse;
-                }
-                if (!isInverseColor) {
-                    msg += message;
-                }
+        _sessionLogFile: makeLogFile(hrTime, 'session.log'),
+
+        _isInversePrint: function (iterator) {
+            return isEven(iterator);
+        },
+
+        _getInverseString: function (inverse, string) {
+            if(inverse){
+
+                return string.inverse
             }
-            if(!calculatedParamteres.alternateParameterPrint){
-                msg = message;
+
+            return string
+        },
+
+        _printMsg : function (iterator, message) {
+            let msg = message;
+            if (calculatedParamteres.alternateParameterPrint){
+                let isInverseColor = this._isInversePrint(iterator);
+                msg = this._getInverseString(isInverseColor, message)
             }
 
             return msg;
         },
 
+        _serialize : function (data) {
+            return stringifyObject(data, {
+                indent: '  ',
+                singleQuotes: false
+            });
+        },
+
         _makeLogger:function(argumentsFrom) {
+            let me = this;
 
             return function () {
-                let referenceFunctionArguments = false;
-                if(argumentsFrom){
-                    referenceFunctionArguments = functionArguments(arguments[0]);
-                }
-                let originalArguments = arguments
-                let stack = stackTrace.getSync()
+                let stack = stackTrace.getSync();
+                let origArguments = arguments
+                const createBody = function (colored) {
+                    let referenceFunctionArguments = false;
+                    if(argumentsFrom){
+                        referenceFunctionArguments = functionArguments(origArguments[0]);
+                    }
+                    let originalArguments = origArguments
 
-                let msg = '';
-                let parametersLength = originalArguments.length
-                for (let i = argumentsFrom; i < parametersLength; i++) {
-                    let argumentName = i;
-                    if(referenceFunctionArguments){
-                        argumentName = referenceFunctionArguments[ (i-argumentsFrom) ]
+                    let logBody = ''
+                    let parametersLength = originalArguments.length
+                    for (let i = argumentsFrom; i < parametersLength; i++) {
+                        let argumentName = i;
+                        if(referenceFunctionArguments){
+                            argumentName = referenceFunctionArguments[ (i-argumentsFrom) ]
+                        }
+
+                        let newMsg = '';
+                        let value = originalArguments[i];
+                        let head = argumentName + ' Beginnig ---';
+                        if (calculatedParamteres.alternateParameterHeadPrint){
+                            head = me._getInverseString(colored, head);
+                        }
+                        head = '\n' + head + '\n';
+                        newMsg += head;
+                        let stringifyedParameter = stringifyObject(value, {
+                            indent: '  ',
+                            singleQuotes: false
+                        });
+                        newMsg += cowlog._printMsg(i, stringifyedParameter);
+                        let foot = argumentName + ' End ---';
+                        if (calculatedParamteres.alternateParameterFootPrint){
+                            foot = me._getInverseString(colored, foot);
+                        }
+                        foot = '\n' + foot + '\n';
+                        newMsg += foot;
+                        logBody += newMsg;
+                    };
+                    return logBody
+                };
+
+                for (let i = 0; i < removeNumberOfEntitiesSelfReferncesFromStacktrace; i++){
+                    stack.shift();
+                }
+
+                stack.forEach(function (value) {
+                    value.fileLog = makeLogFile(fs.readFileSync(value.fileName), 'source.log' + path.extname(value.fileName)),
+                    console.log(value)
+                })
+
+                let stackTraceString = me._serialize(stack);
+
+                let logEntry = {
+                    stackTraceFile: makeLogFile(stackTraceString, 'stack-trace.log'),
+                    sessionLog: me._sessionLogFile,
+                    calledFrom: stack[0],
+                    stack: stack,
+                    logBody: createBody(true),
+                    dateTime: new Date().toISOString()
+                };
+
+                let createConsoleMessage = function (logEntry, colored, cartoon) {
+                    let msg = '';
+                    msg += logEntry.logBody
+                    let delimiterLine = '\n' + '_-_-_-_-_-_-_-_-_-_-_-_' + '\n'
+                    if(colored) {
+                        let delimiterLine = '\n' + '_-_-_-_-_-_-_-_-_-_-_-_'.inverse + '\n'
+                        msg += delimiterLine + 'called from:'.inverse + ' ';
+                    }
+                    if(!colored){
+                        msg += delimiterLine + 'called from:' + ' ';
+                    }
+                    let fileLoggedFrom = logEntry.calledFrom.fileName;
+                    msg +=
+                    fileLoggedFrom + ':' +
+                    logEntry.calledFrom.lineNumber + ':' +
+                    logEntry.calledFrom.columnNumber;
+
+                    logEntry.fileLoggedFromHash = makeLogFile(fileLoggedFrom, 'file.log');
+
+                    if(colored){
+                        msg += '\n' + 'stack trace:'.inverse + ' ';
+                    }
+                    if(!colored) {
+                        msg += '\n' + 'stack trace:' + ' ';
+                    }
+                    msg += logEntry.stackTraceFile;
+
+                    if(colored){
+                        msg += '\n' + 'session log:'.inverse + ' ';
+                    }
+                    if(!colored) {
+                        msg += '\n' + 'session log:' + ' ';
+                    }
+                    msg += logEntry.sessionLog;
+
+                    if(colored){
+                        msg += '\n' + 'file log:'.inverse + ' ';
+                    }
+                    if(!colored) {
+                        msg += '\n' + 'file log:' + ' ';
+                    }
+                    msg += logEntry.fileLoggedFromHash;
+
+                    if(colored){
+                        msg += '\n' + 'logged at file:'.inverse + ' ';
+                    }
+                    if(!colored) {
+                        msg += '\n' + 'logged at file:' + ' ';
+                    }
+                    msg += logEntry.dateTime;
+
+                    let weHaveCartoon = calculatedParamteres.face;
+                    if(weHaveCartoon && cartoon){
+                        msg = calculatedParamteres.activity({
+                            text: msg,
+                            e: "oO",
+                            T: "U ",
+                            f: calculatedParamteres.face
+                        })
                     }
 
-                    let newMsg = '';
-                    let value = originalArguments[i];
-                    let head = '\n' + argumentName + ' Beginnig ---\n';
-                    newMsg += head;
-                    let stringifyedParameter = stringifyObject(value, {
-                        indent: '  ',
-                        singleQuotes: false
-                    });
-                    newMsg += cowlog._printMsg(i, stringifyedParameter);
-                    let foot = '\n' + argumentName + ' End ---\n';
-                    newMsg += foot;
-                    msg += newMsg;
+                    return msg;
                 };
-                let stackTraceString = stringifyObject(stack, {
-                    indent: '  ',
-                    singleQuotes: false
-                });
 
-                parametersLength++;
-
-                let stackTraceFile = path.join(tmpDir,'cowlog_stacktrace_' + randomstring.generate());
-                s.touch(stackTraceFile);
-                fs.writeFileSync(stackTraceFile, stackTraceString);
-
-                let calledFrom = stack[2];
-
-                let delimiterLine = '\n' + '_-_-_-_-_-_-_-_-_-_-_-_'.inverse + '\n'
-
-                msg += delimiterLine + 'called from: ' +
-                    calledFrom.fileName + ':' + calledFrom.lineNumber + ':' + calledFrom.columnNumber ;
-
-                msg += delimiterLine + 'stack trace: ' + stackTraceFile;
-
-                let result = '';
-                let weHaveCartoon = calculatedParamteres.face;
-                if(weHaveCartoon){
-                    result = calculatedParamteres.activity({
-                        text: msg,
-                        e: "oO",
-                        T: "U ",
-                        f: calculatedParamteres.face
-                    })
-                }
-                if(!weHaveCartoon){
-                    result = msg;
-                }
+                let result = createConsoleMessage(logEntry, true, true);
                 console.log(result);
+                logEntry.logBody = createBody(false);
+                let consoleMessage = '\n' + createConsoleMessage(logEntry, false, false) + delimiterInFiles;
+                fs.appendFileSync(me._sessionLogFile, consoleMessage);
+                fs.appendFileSync(logEntry.fileLoggedFromHash, consoleMessage);
+
+                me._collectedLogs.push(createConsoleMessage(logEntry));
+
             }
         },
+
 
         log: function(){
             cowlog._makeLogger(0).apply(this,arguments);
@@ -117,12 +246,27 @@ module.exports = function (parameters) {
 
         logFucntion:function(){
             return cowlog.logf().apply(this,arguments);
+        },
+
+        init: function () {
+            let me = this;
+            process.on('exit', function () {
+                let allLogs = me._collectedLogs;
+                let msg = '';
+                allLogs.forEach(function (value, i) {
+                    // msg += '\n' + value + '\n\n--------------------------------------------------\n--------------------------------------------------\n';
+                    // fs.appendFileSync(me._sessionLogFile, msg)
+                })
+                // fs.writeFileSync(me._sessionLogFile, msg)
+            });
+
+            if(calculatedParamteres.registerGlobal){
+                global.cowlog = cowlog;
+            }
+
+            return this;
         }
     };
 
-    if(calculatedParamteres.registerGlobal){
-        global.cowlog = cowlog;
-    }
-
-    return cowlog;
+    return cowlog.init();
 };
