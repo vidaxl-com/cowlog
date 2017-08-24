c55f7225c30a5e2c03d3bbbfe46666148374ef22
Imre Toth <i.toth@vidaxl.com>
2017-08-24 08:53:44 +0200

    better logging

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 'use strict';
-
 module.exports = function (parameters) {
     const colors = require('colors');
 
     const calculatedParamteres = require('./app/configParser')(parameters);
-    // const s = require('shelljs');
+    const s = require('shelljs');
     const os = require('os');
     const path = require('path');
+    const randomstring = require("randomstring");
 
     const hrTime = process.hrtime();
 
     const tmpDir = os.tmpdir();
     const cowlogTmpDir = path.join(tmpDir,'cowlog/');
+    const cowlogHashDir = path.join(cowlogTmpDir, 'hashes/');
+    const cowlogDbDir = path.join(cowlogTmpDir, 'db/');
 
     const createConsoleMessage = require('./lib/message-creator');
     const makeLogFile = require('./lib/logfile-creator');
 
+    const low = require('lowdb')
+    const FileSync = require('lowdb/adapters/FileSync')
+
+    let dbPath = path.join(cowlogDbDir, 'db.json');
+    s.mkdir('-p', cowlogDbDir);
+    const adapter = new FileSync(dbPath);
+    const db = low(adapter)
+
+    db.defaults({ sessions: [], logs_sessions: {} }).write();
+
+    const dbDir = cowlogDbDir
+
     let cowlog = {
+
+        _db: db,
+
+        _logs: [],
 
         _fileLogs: {},
 
         _collectedLogs: [],
+
+        _sessionId: randomstring.generate(),
 
         _sessionLogFile: makeLogFile(hrTime, cowlogTmpDir, 'session.log'),
 
@@ -29,11 +49,13 @@ module.exports = function (parameters) {
 
 
         log: function(){
-            return cowlog._makeLogger(0).apply(this,arguments);
+            let returnValue = cowlog._makeLogger(0).apply(this,arguments);
+            return returnValue;
         },
 
         logf: function(){
-            return cowlog._makeLogger(1).apply(this,arguments);
+            let returnValue = cowlog._makeLogger(1).apply(this,arguments);
+            return returnValue;
         },
 
         logFucntion:function(){
@@ -41,6 +63,10 @@ module.exports = function (parameters) {
         },
 
         init: function () {
+            db.get('sessions')
+                .push(cowlog._sessionId)
+                .write()
+
             this._makeLogger = require('./lib/logger')(cowlog, calculatedParamteres);
             let me = this;
 
