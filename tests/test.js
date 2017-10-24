d7b64f9f8b98145e0541cef90b75685a33569d48
Imre Toth <i.toth@vidaxl.com>
2017-10-24 18:00:55 +0200

    + logfile-creator tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
-
-const tmpDir = __dirname + '../tmp/'
+const path = require('path');
+const tmpDir = path.join(__dirname, '../tmp/')
 const abcHash = 'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad'
 
 const appContainer = require('../app/container');
@@ -23,16 +23,10 @@ describe('lib tests', function () {
     it('shall create a logfile', function () {
       let logFileCreator = require('../lib/logfile-creator')(tmpDir);
       let abcHashPath = logFileCreator('abc')
-      abcHashPath.should.be.an('string').that.does.include('../tmp/')
+      abcHashPath.should.be.an('string').that.does.include('/tmp/')
         .and.that.does.include('/7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad')
-        // .and.that.does.include('/ba/')
-
-      console.log(abcHashPath)
-      // let hash = hashCreator('abc')
-      // hash.should.be.a('string')
-      // var hexCheck = new RegExp(/^[0-9A-Fa-f]+$/)
-      // expect(hexCheck.test(hash)).to.equal(true)
-      // hash.should.to.equal('ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad')
+        .and.that.does.include('/ba/')
+        .and.that.does.include('_.log')
     })
   })
 })
