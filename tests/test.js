138a0668dc3054abef71e77b0c3adb6963a27f99
Imre Toth <i.toth@vidaxl.com>
2017-10-31 13:19:54 +0100

    code climate coverage play

--- tests/test.js
+++ tests/test.js
@@ -13,27 +13,29 @@ appContainer['runtime-variables'].calculatedParameters = require('../src/app/con
 
 const expect = require('chai').expect
 require('chai').should()
-describe('lib unit tests', function () {
+describe('cowlog tests', function () {
   this.timeout(50000)
-  describe('hash-creator', function () {
-    it('shall provide a hash', function () {
-      let hashCreator = appContainer['hash-creator']
-      let hash = hashCreator('abc')
-      hash.should.be.a('string')
-      var hexCheck = new RegExp(/^[0-9A-Fa-f]+$/)
-      expect(hexCheck.test(hash)).to.equal(true)
-      hash.should.to.equal(mockData.abcHash)
+  describe('lib unit tests', function () {
+    describe('hash-creator', function () {
+      it('shall provide a hash', function () {
+        let hashCreator = appContainer['hash-creator']
+        let hash = hashCreator('abc')
+        hash.should.be.a('string')
+        var hexCheck = new RegExp(/^[0-9A-Fa-f]+$/)
+        expect(hexCheck.test(hash)).to.equal(true)
+        hash.should.to.equal(mockData.abcHash)
+      })
     })
-  })
 
-  describe('logfile-creator', function () {
-    it('shall create a logfile', function () {
-      let logFileCreator = require('../src/lib/logfile-creator')(tmpDir)
-      let abcHashPath = logFileCreator('abc')
-      abcHashPath.should.be.a('string').that.does.include('/tmp/')
-        .and.that.does.include('/7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad')
-        .and.that.does.include('/ba/')
-        .and.that.does.include('_.log')
+    describe('logfile-creator', function () {
+      it('shall create a logfile', function () {
+        let logFileCreator = require('../src/lib/logfile-creator')(tmpDir)
+        let abcHashPath = logFileCreator('abc')
+        abcHashPath.should.be.a('string').that.does.include('/tmp/')
+          .and.that.does.include('/7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad')
+          .and.that.does.include('/ba/')
+          .and.that.does.include('_.log')
+      })
     })
   })
 
