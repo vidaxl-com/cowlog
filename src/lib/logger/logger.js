657fbc305d2d9a1030821bbef81032f6e5b891cf
Imre Toth <i.toth@vidaxl.com>
2017-11-02 18:20:52 +0000

    logger refactoring

--- src/lib/logger/logger.js
+++ src/lib/logger/logger.js
@@ -9,35 +9,38 @@ module.exports = exports = function (container) {
 
   let messageCreator = container['message-creator']
   let hashCreator = container['hash-creator']
-  let logFileCreator = container['log-file-creator']
-  let runtimeVariables = container['runtime-variables']
-  let loggerPrintHelpers = container['logger-print-helpers']
-  let calculatedParameters = container['calculated-parameters']
+  module.logFileCreator = container['log-file-creator']
+  module.runtimeVariables = container['runtime-variables']
+  module.loggerPrintHelpers = container['logger-print-helpers']
+  module.calculatedParameters = container['calculated-parameters']
   let loggerStackTraceFactory = container['logger-stack-trace-factory']
+  let stackTrace = loggerStackTraceFactory()
+
 
   return function (argumentsFrom) {
+    module.argumentsFrom = argumentsFrom
     return function () {
-      let origArguments = arguments
-      let stackTrace = loggerStackTraceFactory()
-      let stackTraceString = stackTrace.stackTraceString
-      let stack = stackTrace.stack
-      let logEntry = module.createLogEntry(stackTraceString, stack, argumentsFrom, origArguments, calculatedParameters, loggerPrintHelpers, logFileCreator, runtimeVariables)
+      module.stackTraceString = stackTrace.stackTraceString
+      module.stack = stackTrace.stack
+      module.origArguments = arguments
+
+      let logEntry = module.createLogEntry()
       let returnLevel = 0
       let returnFunction = function (options) {
         returnLevel++
         let returnValue = returnFunction
         logEntry.hashes = logEntry.hashes || []
-        let returnValue_ = module.evaluateReturnFunctionOptions(options, logEntry, runtimeVariables, origArguments)
+        let returnValue_ = module.evaluateReturnFunctionOptions(options, logEntry, module.origArguments)
         if(returnValue_) {
           returnValue = returnValue_
         }
         if (returnLevel === 1) {
-          let result = messageCreator(calculatedParameters, logEntry, true, true)
+          let result = messageCreator(module.calculatedParameters, logEntry, true, true)
           console.log(result.toString())
-          logEntry.logBody = createBody(false, argumentsFrom, origArguments, calculatedParameters, loggerPrintHelpers)
-          let consoleMessage = '\n' + messageCreator(calculatedParameters, logEntry, false, false) + delimiterInFiles
-          fs.appendFileSync(runtimeVariables.sessionLogFile, consoleMessage)
-          runtimeVariables.collectedLogs.push(messageCreator(calculatedParameters, logEntry, false, false))
+          logEntry.logBody = createBody(false, null, module.origArguments, module.calculatedParameters, module.loggerPrintHelpers)
+          let consoleMessage = '\n' + messageCreator(module.calculatedParameters, logEntry, false, false) + delimiterInFiles
+          fs.appendFileSync(module.runtimeVariables.sessionLogFile, consoleMessage)
+          module.runtimeVariables.collectedLogs.push(messageCreator(module.calculatedParameters, logEntry, false, false))
         }
 
         return returnValue
@@ -48,40 +51,40 @@ module.exports = exports = function (container) {
   }
 }
 
-module.createLogEntry = function (stackTraceString, stack, argumentsFrom, origArguments, calculatedParameters, loggerPrintHelpers, logfileCreator, runtimeVariables) {
+module.createLogEntry = function () {
   return {
-    stackTraceFile: logfileCreator(stackTraceString, 'stack-trace.log'),
-    sessionLog: runtimeVariables.sessionLogFile,
-    calledFrom: stack[0],
-    stack: stack,
-    logBody: createBody(true, argumentsFrom, origArguments, calculatedParameters, loggerPrintHelpers),
+    stackTraceFile: module.logFileCreator(module.stackTraceString, 'stack-trace.log'),
+    sessionLog: module.runtimeVariables.sessionLogFile,
+    calledFrom: module.stack[0],
+    stack: module.stack,
+    logBody: createBody(true, module.argumentsFrom, module.origArguments, module.calculatedParameters, module.loggerPrintHelpers),
     dateTime: new Date().toISOString()
   }
 }
 
-module.evaluateReturnFunctionOptions = function (options, logEntry, runtimeVariables, origArguments) {
+module.evaluateReturnFunctionOptions = function (options, logEntry) {
   let returnValue = false;
   if (options) {
     module.die(options)
-    module.lasts(options, runtimeVariables, logEntry)
-    module.last(options, runtimeVariables, logEntry)
+    module.lasts(options, module.runtimeVariables, logEntry)
+    module.last(options, module.runtimeVariables, logEntry)
     if (options === 'return') {
-      returnValue = (origArguments[origArguments.length - 1])
+      returnValue = (module.origArguments[module.origArguments.length - 1])
     }
   }
   return returnValue
 }
 
-module.lasts = function (options, runtimeVariables, logEntry) {
+module.lasts = function (options, logEntry) {
   if (options === 'lasts') {
-    runtimeVariables.lastLogs = runtimeVariables.lastLogs || []
-    runtimeVariables.lastLogs.push(logEntry)
+    module.runtimeVariables.lastLogs = module.runtimeVariables.lastLogs || []
+    module.runtimeVariables.lastLogs.push(logEntry)
   }
 }
 
-module.last = function (options, runtimeVariables, logEntry) {
+module.last = function (options, logEntry) {
   if (options === 'last') {
-    runtimeVariables.lastLogs = [logEntry]
+    module.runtimeVariables.lastLogs = [logEntry]
   }
 }
 
