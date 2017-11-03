468fc226bbc340275fd00fe4447163ce9e7f00df
Imre Toth <i.toth@vidaxl.com>
2017-11-03 14:35:51 +0100

    better configParser.js

--- src/app/configParser/configParser.js
+++ src/app/configParser/configParser.js
@@ -3,21 +3,15 @@ const isObject = require('is-object')
 const merge = require('merge')
 
 module.exports = exports = function (parameters) {
-  let defaultParameters = require('../configs/default')
-  if (!parameters) {
-    parameters = defaultParameters
-  }
-  return module.processParameters(parameters)
-}
-
-module.processParameters = function (parameters) {
+  let defaultParameters = module.parameterRequest('default')
+  parameters = parameters || module.parameterRequest('default')
   return module.parameterRequest(parameters)
 }
 
 module.parameterRequest = function (requestEntity) {
-    if (isString(requestEntity)) {
-      requestEntity = require(`../configs/${requestEntity}`)
-    }
+  if (isString(requestEntity)) {
+    requestEntity = require(`../configs/${requestEntity}`)
+  }
 
   return requestEntity
 }
