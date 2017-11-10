7937783f29295d2d21317280ba83ab4462021ce0
Imre Toth <i.toth@vidaxl.com>
2017-11-10 18:06:03 +0100

    code cleanup

--- tests/lib/test-runner.js
+++ tests/lib/test-runner.js
@@ -1,5 +1,4 @@
 const cowlog = require('./cowlog-provider')()
-const printPresentationInfo = !process.env.mocha
 const blockLogOutput = require('kidnap-console').blockLogOutput
 
 module.exports = exports = function (logf) {
@@ -13,21 +12,11 @@ module.exports = exports = function (logf) {
 
       if (logf) {
         output = blockLogOutput(() => {
-          // if (printPresentationInfo) {
-          //   console.log('console.logf.apply (this, ', origArguments, ') \noutput:\n')
-          //   console.log.apply(this, arguments)
-          //   console.log('***********************')
-          // }
           returnValue = cowlog.logf.apply(this, origArguments)
         })
       }
       if (!logf) {
         output = blockLogOutput(() => {
-          // if (printPresentationInfo) {
-          //   console.log('console.log.apply (this, ', origArguments, ') \noutput:\n')
-          //   console.log.apply(this, arguments)
-          //   console.log('***********************')
-          // }
           returnValue = cowlog.log.apply(this, origArguments)
         })
       }
