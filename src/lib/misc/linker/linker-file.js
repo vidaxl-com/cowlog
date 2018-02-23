54188f5e71d3fee17acd269b7798ded72613186a
Imre Toth <tothimre@gmail.com>
2018-02-23 19:42:32 +0100

    The linker has an internal file blacklist.

--- src/lib/misc/linker/linker-file.js
+++ src/lib/misc/linker/linker-file.js
@@ -1,22 +1,32 @@
 const fs = require('fs')
 
 module.exports = exports = function (file, beginning, closing, newValue = null) {
-  let string = fs.readFileSync(file, {encoding: 'utf8'})
-  let result = require('./linker')(string, beginning, closing, newValue)
-  let writeFile = (result !== string)
+  const blackList = ['.git', '.svn', 'node_modules', 'dist']
+  let blackListed = false
+  blackList.forEach(function (pathPart) {
+    if(!blackListed && !file.includes(pathPart)){
+    } else {
+      blackListed = true;
+    }
+  })
+  if(!blackListed) {
+    let string = fs.readFileSync(file, {encoding: 'utf8'})
+    let result = require('./linker')(string, beginning, closing, newValue)
+    let writeFile = (result !== string)
 
-  if(newValue){
-    if (writeFile) {
-      fs.writeFileSync(file, result)
+    if (newValue) {
+      if (writeFile) {
+        fs.writeFileSync(file, result)
+
+        return result
+      }
+    }
+
+    if (!newValue && result) {
 
       return result
     }
   }
-
-  if (!newValue && result) {
-
-    return result
-  }
-
+:
   return ''
 }
