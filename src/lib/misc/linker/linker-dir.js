85c7e7fd3c6b07f18abbbf46edf38e5bbf5e0f07
Imre Toth <tothimre@gmail.com>
2018-02-23 06:57:46 +0100

    tests are running

--- src/lib/misc/linker/linker-dir.js
+++ src/lib/misc/linker/linker-dir.js
@@ -1,12 +1,12 @@
 const readdir = require('recursive-readdir-sync')
 const linkerFile = require('./linker-file')
 
-module.exports = exports = function (dir, beginning, closing, newValue) {
+module.exports = exports = function (dir, beginning, closing, newValue = null) {
   let files = readdir(dir)
   files = files || []
   let returnValue = {}
-  files.forEach(function (file) {
 
+  files.forEach(function (file) {
     let result = linkerFile(file, beginning, closing, newValue)
     if(result){
       returnValue[file] = result
