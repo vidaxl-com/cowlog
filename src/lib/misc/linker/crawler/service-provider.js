e365b40d08e649a0f949103862e1c44cd36f1340
Imre Toth <tothimre@gmail.com>
2018-02-27 00:31:14 +0100

    Better hash generation, more data at the service-provider.

--- src/lib/misc/linker/crawler/service-provider.js
+++ src/lib/misc/linker/crawler/service-provider.js
@@ -1,36 +1,83 @@
 const unique = require('array-unique')
-const sha256 = require('sha256')
 const objectHash = require('object-hash')
-module.exports = exports = function (crawlerData, hash = null) {
+const objectPath = require('object-path')
+const sha256 = require('sha256')
+
+module.exports = exports = function (crawlerData, result = null) {
+  let hashes = module.getHash(result)
+
   let serviceMap = {}
   let serviceParametersMap = {}
+  let fullDataTree = {}
 
   crawlerData.forEach(function (data) {
     let entries = data.entries
     entries.forEach(function (entry) {
+      objectPath.set(fullDataTree, entry.parameters.join('.'), true)
       let parameters = entry.parameters
       let lastParameter = parameters[parameters.length - 1]
       if (parameters.length >= 2 && lastParameter === 'begin') {
         let extractedParameters = parameters.slice(0, (parameters.length - 2)).join(' ')
         if (extractedParameters) {
-          let firstParameter = parameters[0]
+          let categoryName = parameters[0]
+
           let serviceParameters = parameters.slice(1, parameters.length - 1).join(' ')
-          let arr = serviceParametersMap[firstParameter]
-          if (!arr) {
-            arr = serviceParametersMap[firstParameter] = []
+          //services
+          let serviceArray = serviceParametersMap[categoryName]
+          let serviceDelimiterArray = serviceParametersMap[categoryName]
+
+          if (!serviceArray) {
+            serviceArray = serviceParametersMap[categoryName] = []
+
           }
-          arr.push(serviceParameters)
-          serviceMap[firstParameter] = arr
+          serviceArray.push(serviceParameters)
+          serviceMap[categoryName] = serviceArray
+
+          //serviceDelimiters
+          // if(objectPath.has(serviceDelimiterMap))
+          // if(serviceDelimiterMap)
+          // let arr = serviceParametersMap[categoryName]
+          // if (!arr) {
+          //   arr = serviceParametersMap[categoryName] = []
+          // }
+          // arr.push(serviceParameters)
+          // serviceMap[categoryName] = arr
+
         }
       }
     })
   })
-  let currentHash = sha256(objectHash(serviceMap))
+
+  let servicesContainer = {}
   Object.keys(serviceMap).forEach(function (key) {
-    serviceMap[key] = unique(serviceMap[key])
+    servicesContainer[key] = unique(serviceMap[key])
   })
-  if (currentHash !== hash) {
-    exports(crawlerData, currentHash)
+
+  let services = {
+    fullDataTree: fullDataTree,
+    services: servicesContainer,
+    'services.delimiters':{}
   }
-  l(serviceMap)
+  let currentHashes = module.getHash(services)
+
+  if (currentHashes.services !== hashes.services) {
+    exports(crawlerData, services)
+
+    return null
+  }
+  l(services, currentHashes)
+}
+
+module.getHash = function (result) {
+  let services = objectHash(objectPath.get(result, 'services') || '')
+  let fullDataTree = objectHash(objectPath.get(result, 'FullDataTree') || '')
+  if (services && fullDataTree) {
+    return {
+      services: services,
+      fullDataTree: fullDataTree,
+      all: sha256(services + fullDataTree)
+    }
+  }
+
+  return {}
 }
