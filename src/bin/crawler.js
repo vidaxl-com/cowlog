40908be28bdc0a225c68c5e70d42c05bc0f02d4d
Imre Toth <tothimre@gmail.com>
2018-03-04 12:34:29 +0100

    Better recursion for the service-provider.

--- src/bin/crawler.js
+++ src/bin/crawler.js
@@ -2,6 +2,6 @@
 
 let path = require('path')
 let projectRoot = path.join(__dirname, '../../')
-let crawlerData = require('../lib/misc/linker/crawler/crawler')(projectRoot)
-let data = require('../lib/misc/linker/crawler/service-provider')(crawlerData)
+// let crawlerData = require('../lib/misc/linker/crawler/crawler')(projectRoot)
+let data = require('../lib/misc/linker/crawler/service-provider')(projectRoot)
 l(data)
--- src/lib/misc/linker/crawler/service-provider.js
+++ src/lib/misc/linker/crawler/service-provider.js
@@ -1,23 +1,63 @@
 const unique = require('array-unique')
 const sha256 = require('sha256')
 const objectHash = require('object-hash')
-module.exports = exports = function (crawlerData, hash = null) {
+const objectPath = require('object-path')
+const flatten = require('flat')
+
+module.exports = exports = function (projectRoot, metaData = {}) {
+  let crawlerData = require('./crawler')(projectRoot)
+  let newMetaData = exports.hashIt(exports.extractData(crawlerData))
+  let oldHashes = metaData['hashes']
+  let newHashes = newMetaData['hashes']
+  // let oldServiceMapHash = objectPath(crawlerData, 'metaData.serviceMap')
+
+  let returnValue = ''
+
+  if (!oldHashes || oldHashes.all !== newHashes.all) {
+    returnValue = exports(projectRoot, newMetaData)
+  }
+  else {
+    returnValue = newMetaData
+  }
+
+  return returnValue
+}
+
+exports.hashIt = function (data) {
+  let keys = Object.keys(data)
+  let hashes = {}
+  keys.forEach(function (key) {
+    let subDataValue = data[key];
+    hashes[key] = objectHash(subDataValue)
+  })
+  let all = ''
+  Object.keys(hashes).forEach(function (key) {
+    all += hashes[key]
+  })
+  all = sha256(all)
+  hashes.all = all
+
+  return {data, hashes}
+}
+
+exports.extractData = function (crawlerData) {
   let serviceMap = {}
-  let serviceParametersMap = {}
+  let serviceDelimiterMap = {}
 
   crawlerData.forEach(function (data) {
     let entries = data.entries
     entries.forEach(function (entry) {
       let parameters = entry.parameters
+      // Check the begin and the end tag opener and closer.
       let lastParameter = parameters[parameters.length - 1]
       if (parameters.length >= 2 && lastParameter === 'begin') {
         let extractedParameters = parameters.slice(0, (parameters.length - 2)).join(' ')
         if (extractedParameters) {
           let firstParameter = parameters[0]
           let serviceParameters = parameters.slice(1, parameters.length - 1).join(' ')
-          let arr = serviceParametersMap[firstParameter]
+          let arr = serviceDelimiterMap[firstParameter]
           if (!arr) {
-            arr = serviceParametersMap[firstParameter] = []
+            arr = serviceDelimiterMap[firstParameter] = []
           }
           arr.push(serviceParameters)
           serviceMap[firstParameter] = arr
@@ -25,12 +65,9 @@ module.exports = exports = function (crawlerData, hash = null) {
       }
     })
   })
-  let currentHash = sha256(objectHash(serviceMap))
-  Object.keys(serviceMap).forEach(function (key) {
-    serviceMap[key] = unique(serviceMap[key])
-  })
-  if (currentHash !== hash) {
-    exports(crawlerData, currentHash)
-  }
-  l(serviceMap)
+
+  return {serviceMap, serviceDelimiterMap, }
+}
+
+module.get = function(data){
 }
