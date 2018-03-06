1d0037eef464dcdc88b31d6297726efa3e787be6
Imre Toth <tothimre@gmail.com>
2018-03-06 14:59:51 +0100

    improving /fixing tests

---
+++ tests/tests/unit/linker/unit-tests-linker-dir.js
@@ -0,0 +1,43 @@
+/* eslint-env mocha */
+require('../../../lib/test-common')
+const path = require('path')
+const fs = require('fs')
+const copyFileSync = require('fs-copy-file-sync')
+const appContainer = require(`../../../../src/app/container`)()
+const readmeFileName = appContainer.readmeFileName
+require('../../../../src/index')()
+require('chai').should()
+
+describe('Testing', function () {
+  describe('@linker-dir', function () {
+    it('test @linker-dir-template', function () {
+      let linker = require('../../../../src/lib/misc/linker/linker-dir')
+      let tmpdir = path.join(process.cwd(), 'tmp')
+      let tmpFile = path.join(tmpdir, readmeFileName)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile + 'copy')
+      let fileNames = linker(tmpdir, '<!--- example begin -->', '<!--- example end -->', '+++***---')
+      if (!Object.keys(fileNames)[0]){
+        throw "As a result you have at least one file changed by now"
+      }
+
+    })
+
+    it('test @linker-dir-return', function () {
+      let linker = require('../../../../src/lib/misc/linker/linker-dir')
+      let tmpdir = path.join(process.cwd(), 'tmp')
+      let tmpFile = path.join(tmpdir, readmeFileName)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile + 'copy')
+      let oldCotent = fs.readFileSync(tmpFile, {encoding: 'utf8'})
+      let results = linker(tmpdir, '<!--- example begin -->', '<!--- example end -->')
+      if (!oldCotent.includes(results)) {
+        throw String('There is stg fishy going on!')
+      }
+      let newCotent = fs.readFileSync(tmpFile, {encoding: 'utf8'})
+      newCotent.should.be.equal(oldCotent)
+      //todo: test it!
+    })
+
+  })
+})
---
+++ tests/tests/unit/linker/unit-tests-linker-file.js
@@ -0,0 +1,53 @@
+/* eslint-env mocha */
+require('../../../lib/test-common')
+const path = require('path')
+const fs = require('fs')
+const copyFileSync = require('fs-copy-file-sync')
+const appContainer = require(`../../../../src/app/container`)()
+const readmeFileName = appContainer.readmeFileName
+require('../../../../src/index')()
+
+const expect = require('chai').expect
+require('chai').should()
+
+describe('Testing', function () {
+  describe('@linker-file', function () {
+    let linker = require('../../../../src/lib/misc/linker/linker-file')
+    it('changed content', function () {
+      let tmpFile = path.join(process.cwd(), 'tmp', readmeFileName)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile)
+      let result = linker(tmpFile, '<!--- example begin -->', '<!--- example end -->', '+++')
+
+      let {changed} = result.meta
+
+      expect(changed.all).to.equal(true)
+      expect(changed.withoutWhiteSpaces).to.equal(true)
+      expect(changed.status).to.equal('write')
+    })
+
+    it('not changed content', function () {
+      let tmpFile = path.join(process.cwd(), 'tmp', readmeFileName)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile)
+      let result = linker(tmpFile, '<!-- example begin -->', '<!-- example end -->', '+++')
+      let {changed} = result.meta
+
+      expect(changed.all).to.equal(false)
+      expect(changed.withoutWhiteSpaces).to.equal(false)
+      expect(changed.status).to.equal('read')
+    })
+
+    it('return selected content', function () {
+      let tmpFile = path.join(process.cwd(), 'tmp', readmeFileName)
+      copyFileSync(path.join(process.cwd(), readmeFileName), tmpFile)
+      let oldCotent = fs.readFileSync(tmpFile, {encoding: 'utf8'})
+      let result = linker(tmpFile, '<!--- example begin -->', '<!--- example end -->')
+      let {changed} = result.meta
+
+      expect(changed.all).to.equal(false)
+      expect(changed.withoutWhiteSpaces).to.equal(false)
+      expect(changed.status).to.equal('read')
+      oldCotent.should.be.a('string').that.does.include(result.returnData)
+    })
+  })
+})
+
