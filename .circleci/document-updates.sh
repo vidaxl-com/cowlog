96b5f7d897c2062665f8cc2b75b974ef637b6383
Imre Toth <i.toth@vidaxl.com>
2018-03-11 22:41:05 +0100

    First initial commit to the new master branch.
    This is the first commit before the release it contains all relevant changes
    happened during the project. We create this branch to reset the project log.
    The old master branch it at the archives/old-flow.
    
    The new master branch is managed with Lerna; this is a multi-package
    Repository manager. With his tool we can efficiently manage versioning
    packages that are co-dependent; still, you want to share useful
    functionality with others, so the best of the conquering techniques.
    You don't have to compromise if you want your monolithic system
    operation in a smart way (cathedral-style development) and yet
    be able the share chunks of finely grained functionalities to the masses.

--- .circleci/document-updates.sh
+++ .circleci/document-updates.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+./packages/cowlog/src/bin/doc-crawler ../../
+
+git status
 #SUCCESS=0
 #
 #if [ "$SUCCESS" -eq "0" ]
@@ -8,9 +11,6 @@
 #    then
         git config --global user.email "i.toth@vidaxl.com"
         git config --global user.name "Imre Toth"
-        packages/cowlog/src/bin/doc-crawler
-#        git checkout  packages/cowlog/tests/directory-fixtures/
-#        git checkout  packages/cowlog/cowlog
         git checkout packages/cowlog/tests/directory-fixtures/
         git add *.md
         git status
@@ -22,8 +22,7 @@
 
             echo "$currentBranch Zizi   "
 
-            git pull origin $current
-            Branch
+            git pull origin $currentBranch
             git push origin $currentBranch && echo "Released" || exit 0
         else
             echo "Clean git repository."
@@ -33,3 +32,5 @@
 #    echo "No Release"
 #    exit 1
 #fi
+
+git status
---
+++ .circleci/prepare-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+
+tree ./
+export NVM_DIR="$HOME/.nvm"
+
+if [ ! -d "NVM_DIR" ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+fi
+
+if [ ! -d "node_modules" ]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    nvm install 9.8.0
+    nvm use 9.8.0
+    npm build
+    nvm use system
+    npm install
+fi
+
+if [ ! -d "packages/cowlog/node_modules" ]; then
+    npm run i
+fi
+
+
+
