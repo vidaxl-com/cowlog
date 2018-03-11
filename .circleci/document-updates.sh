862d73d60f3860d062895c0dfd1514f329ba7345
Imre Toth <i.toth@vidaxl.com>
2018-03-11 22:41:05 +0100

    First initial commit to the new master branch.
    This is the first commit before the release it contains all relevant changes
    happened during the project. We create this branch to reset the project log.
    The old master branch it at the archives/old-flow.
    
    The new master branch is managed with Lerna; this is a multi-package
    Repository manager. With his tool we can efficiently manage versioning
    packages that are co-dependent; still, you want to share useful
    functionality with others, so the best of the conquering techniques.
    You don't have to compromise if you want your monolithic system
    operation in a smart way (cathedral-style development) and yet
    be able the share chunks of finely grained functionalities to the masses.

--- .circleci/document-updates.sh
+++ .circleci/document-updates.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+./packages/cowlog/src/bin/doc-crawler ../../
+
+git status
 #SUCCESS=0
 #
 #if [ "$SUCCESS" -eq "0" ]
 #then
 #    if [ "$CIRCLE_JOB" == "node7" ]
 #    then
+     if [ -f "/tmp/test" ];then
         git config --global user.email "i.toth@vidaxl.com"
         git config --global user.name "Imre Toth"
-        packages/cowlog/src/bin/doc-crawler
-#        git checkout  packages/cowlog/tests/directory-fixtures/
-#        git checkout  packages/cowlog/cowlog
         git checkout packages/cowlog/tests/directory-fixtures/
         git add *.md
         git status
@@ -19,17 +20,16 @@
             git commit --author="CircleCi <i.toth@vidaxl.com>" -a -m "`git log -1` [ci skip][release]" &&
             test=`git branch | grep "*"` &&
             currentBranch=${test:2}
-
-            echo "$currentBranch Zizi   "
-
-            git pull origin $current
-            Branch
+            git pull origin $currentBranch
             git push origin $currentBranch && echo "Released" || exit 0
         else
             echo "Clean git repository."
         fi
+    fi
 #    fi
 #else
 #    echo "No Release"
 #    exit 1
 #fi
+
+git status
