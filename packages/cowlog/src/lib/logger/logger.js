453bc707359d691dac854712c32d0890c19b4bf3
Imre Toth <i.toth@vidaxl.com>
2018-06-13 14:44:45 +0200

    cleaned currying

--- packages/cowlog/src/lib/logger/logger.js
+++ packages/cowlog/src/lib/logger/logger.js
@@ -1,5 +1,6 @@
 'use strict'
 const fs = require('fs')
+const _ = require('lodash')
 
 module.exports = exports = function (container) {
   let messageCreator = container['message-creator']
@@ -9,26 +10,20 @@ module.exports = exports = function (container) {
   module.calculatedParameters = container['calculated-parameters']
   const createBody = container['logger-body-factory']
   const dictionary = module.dictionary = container.dictionary
+  const loggerStackTraceFactory = container['logger-stack-trace-factory']
+  const stackTrace = loggerStackTraceFactory()
+  const stack = stackTrace.stack
 
-  let cb = (e, d) => console.log(d,"FAKKKKK")
-
-  return require('../unlimited-curry/index')('END',cb)
-
-  return function (argumentsFrom) {
-    module.argumentsFrom = argumentsFrom
+  let mainFunction = function (argumentsFrom) {
     return function () {
-      let loggerStackTraceFactory = container['logger-stack-trace-factory']
-      let stackTrace = loggerStackTraceFactory()
-      module.stackTraceString = stackTrace.stackTraceString
-      module.stack = stackTrace.stack
-      module.origArguments = arguments
-      let logEntry = module.createLogEntry(createBody)
+      const origArguments = arguments
+      const logEntry = module.createLogEntry(createBody, argumentsFrom, stackTrace.stackTraceString, stack, origArguments)
       let returnLevel = 0
-      let returnFunction = function (command) {
+      const returnFunction = function (command) {
         returnLevel++
         let returnValue = returnFunction
         logEntry.hashes = logEntry.hashes || []
-        let returnValue_ = module.evaluateReturnFunctionOptions(command, logEntry)
+        let returnValue_ = module.evaluateReturnFunctionOptions(command, logEntry, origArguments)
         // console.log(returnValue_)
         if (returnValue_) {
           returnValue = returnValue_
@@ -36,7 +31,7 @@ module.exports = exports = function (container) {
         if (returnLevel === 1) {
           let result = messageCreator(module.calculatedParameters, logEntry, true, true)
           console.log(result.toString())
-          logEntry.logBody = createBody(false, argumentsFrom, module.origArguments, module.calculatedParameters, module.loggerPrintHelpers)
+          logEntry.logBody = createBody(false, argumentsFrom, origArguments, module.calculatedParameters, module.loggerPrintHelpers)
           let consoleMessage = '\n' + messageCreator(module.calculatedParameters, logEntry, false, false) +
             dictionary.delimiterInFiles
           fs.appendFileSync(module.runtimeVariables.sessionLogFile, consoleMessage)
@@ -49,27 +44,29 @@ module.exports = exports = function (container) {
       return returnFunction()
     }
   }
+  _.throttle(mainFunction)
+  return mainFunction
 }
 
-module.createLogEntry = function (bodyFactory) {
+module.createLogEntry = function (bodyFactory, argumentsFrom, stackTraceString, stack, origArguments) {
   return {
-    stackTraceFile: module.logFileCreator(module.stackTraceString, 'stack-trace.log'),
+    stackTraceFile: module.logFileCreator(stackTraceString, 'stack-trace.log'),
     sessionLog: module.runtimeVariables.sessionLogFile,
-    calledFrom: module.stack[0],
-    stack: module.stack,
-    logBody: bodyFactory(true, module.argumentsFrom, module.origArguments, module.calculatedParameters, module.loggerPrintHelpers),
+    calledFrom: stack[0],
+    stack: stack,
+    logBody: bodyFactory(true, argumentsFrom, origArguments, module.calculatedParameters, module.loggerPrintHelpers),
     dateTime: new Date().toISOString()
   }
 }
 
-module.evaluateReturnFunctionOptions = function (command, logEntry) {
+module.evaluateReturnFunctionOptions = function (command, logEntry, origArguments) {
   let returnValue = false
   if (command) {
     module.die(command)
     module.lasts(command, logEntry)
     module.last(command, logEntry)
     if (command === module.dictionary.return) {
-      returnValue = (module.origArguments[module.origArguments.length - 1])
+      returnValue = (origArguments[origArguments.length - 1])
     }
   }
   return returnValue
