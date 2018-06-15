ab594b32b6c214fde5ff7fdc9054b2a30f29f077
Imre Toth <i.toth@vidaxl.com>
2018-06-15 19:23:56 +0200

    promise support for unliited curry first steps

--- packages/cowlog/src/lib/unlimited-curry/index.js
+++ packages/cowlog/src/lib/unlimited-curry/index.js
@@ -1,13 +1,12 @@
 const _ = require('lodash')
+const debounce = require('es6-promise-debounce');
+require('cowlog')()
 
 const getFrom = function (from, dataArgument = null) {
   let workData = dataArgument || this.data
-
   let returnArrayChunks = workData.returnArrayChunks.slice(from)
-
   returnArray = []
   returnArrayChunks.forEach(chunkData=>chunkData.forEach(pieceData=>returnArray.push(pieceData)))
-
   const data = {returnArray, returnArrayChunks}
   returnObject = {data, getFrom}
 
@@ -36,10 +35,49 @@ const callback = function (cb) {
   return caller(returnArray)
 }
 
-const promise = function(){
-  return promisify(callback)
+
+const promise = async function (cb) {
+  let debouncedFunction = _.debounce((data)=>{
+    ll(data)
+    cb(0,data)
+  }, 0)
+  let level = 0
+  returnArray = []
+  returnArrayChunks = []
+  const caller = function(ra) {
+    if(!level){
+      level++
+      return caller
+    }
+    const callerArguments = Array.from(arguments)
+    returnArrayChunks.push(callerArguments)
+    debouncedFunction(getFrom(0,{returnArrayChunks}))
+
+    level++
+    return caller
+  }
+  return caller(returnArray)
 }
 
+
 module.exports = exports = {
-  callback
+  callback,
+  promise
 }
+
+// promise(function (e,data) {ll(data)})(111)
+ll(promise(()=>{})
+  .then(callMe=>{
+      ll(callMe)
+      callMe('YEY')('yuy')
+    },
+    (d)=>{}))
+
+// callback(()=>{})()
+
+let debouncedFunction = debounce(function() {
+  // ll(11111111111)
+  return new Promise(function(resolve) {
+    resolve();
+  },()=>{});
+}, 1);
