d30f761075a318a1326ff6dfea821d2538ebb10c
Imre Toth <i.toth@vidaxl.com>
2018-06-18 18:29:41 +0200

    Does not add final empty arguments to the returnArrayChunks.

--- packages/cowlog/src/lib/unlimited-curry/src/index.js
+++ packages/cowlog/src/lib/unlimited-curry/src/index.js
@@ -13,26 +13,26 @@ const getFrom = function (from, dataArgument = null) {
 }
 
 const detached = function (cb) {
-  let debouncedFunction = _.debounce((data)=>{
-    cb(0,data)
-  }, 0)
-  let level = 0
-  returnArray = []
-  returnArrayChunks = []
-  const caller = function(ra) {
-    if(!level){
-      level++
-      return caller
-    }
-
-    const callerArguments = Array.from(arguments)
-    returnArrayChunks.push(callerArguments)
-    debouncedFunction(getFrom(0,{returnArrayChunks}))
-
-    level++
-    return caller
-  }
-  return caller(returnArray)
+  // let debouncedFunction = _.debounce((data)=>{
+  //   cb(0,data)
+  // }, 0)
+  // let level = 0
+  // returnArray = []
+  // returnArrayChunks = []
+  // const caller = function(ra) {
+  //   if(!level){
+  //     level++
+  //     return caller
+  //   }
+  //
+  //   const callerArguments = Array.from(arguments)
+  //   returnArrayChunks.push(callerArguments)
+  //   debouncedFunction(getFrom(0,{returnArrayChunks}))
+  //
+  //   level++
+  //   return caller
+  // }
+  // return caller(returnArray)
 }
 
 const sync = function (cb) {
@@ -47,8 +47,9 @@ const sync = function (cb) {
       return caller
     }
     const callerArguments = Array.from(arguments)
-    returnArrayChunks.push(callerArguments)
-
+    if(level && callerArguments.length){
+      returnArrayChunks.push(callerArguments)
+    }
     if(!notEmpty){
       level = 0
       let data = getFrom(0, {returnArrayChunks})
--- packages/cowlog/src/lib/unlimited-curry/tests/tests/test.js
+++ packages/cowlog/src/lib/unlimited-curry/tests/tests/test.js
@@ -3,38 +3,42 @@ const assert = require('chai').assert
 const expect = require('chai').expect
 require('chai').should()
 const {sync} = require('../../src/index')
-// require('cowlog')()
+require('cowlog')()
 
 describe('sync tests', function () {
-  this.timeout(150000)
+
   const a = sync()
-  const b = a(1,2,3,4,5)('a','b','c')()
+  const b = a(1, 2, 3, 4, 5)('a', 'b', 'c')()
 
   it('basic test without callback', function () {
-    // const a = uc()
-    // ll(sync,a,b)
-
     let all = require('../../src/index')
-    // ll(all,"F")('die')
     expect(all).to.be.an('object')
-      // .that.include.all.keys('sync', 'detached')
       .that.have.all.keys(['sync', 'detached'])
-
     expect(b).to.be.an('object').that.have.all.keys('data', 'getFrom')
-
   })
 
   it('tests a', function () {
-    expect(a(()=>{})).to.be.a('function')
+    expect(a(() => {})).to.be.a('function')
   })
-  it('tests the output of a', function () {
-    expect(a(()=>{})).to.be.a('function')
-  })
-  it('tests the output of calling a with an empty function call', function () {
-    // ll(a(()=>{})())
-    expect(a(()=>{})()).to.be.an('object')
-    l(a(()=>{})())('die')
-  })
+  describe('sync return tests', function () {
+    this.timeout(150000)
 
+    it('tests the output of a', function () {
+      l(b)
+      expect(a(() => {})).to.be.a('function')
+    })
+  })
+  describe('sync callback tests', function () {
+    this.timeout(150000)
 
+    it('tests a', function () {
+      expect(a(() => {})).to.be.a('function')
+    })
+    it('tests the output of a', function () {
+      expect(a(() => {})).to.be.a('function')
+    })
+    it('tests the output of calling a with an empty function call', function () {
+      expect(a(() => {})()).to.be.an('object')
+    })
+  })
 })
