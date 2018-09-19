c13d6e8dcee8313db42337d74ba17a9aa5d055e7
Imre Toth <i.toth@vidaxl.com>
2018-09-19 02:21:58 +0200

    better stucture and better namings in the unlimited-curry

---
+++ packages/unlimited-curry/src/unlimited-curry-factory/index.js
@@ -0,0 +1,127 @@
+const RETURN_FROM_PROMISE = 1
+const RETURN_FROM_CALLBACK = 0
+// require('cowlog')()
+const getFrom = function (from, dataArgument) {
+  let workData = dataArgument
+  let returnArrayChunks = workData.returnArrayChunks.slice(from)
+  let returnArray = []
+  returnArrayChunks.forEach(chunkData => chunkData.forEach(pieceData => returnArray.push(pieceData)))
+  const data = { returnArray, returnArrayChunks }
+  let returnObject = { data, getFrom }
+  returnObject.command = require('./command-parser')(returnObject)
+
+  return returnObject
+}
+
+const safetyExecutor = function safetyExecutor (data, callback) {
+  let timeoutSate = null
+  if (callback && typeof callback === 'function') {
+    timeoutSate = setTimeout(callback, 0, 2, data)
+  }
+  return timeoutSate
+}
+module.exports = exports = (paramters = false) => function (callback) {
+  // l(paramters)()
+  let timeoutSate = null
+  let level = 0
+  let returnArray = []
+  let returnArrayChunks = []
+
+  const state = {
+    timeoutSate,
+    level,
+    returnArray,
+    returnArrayChunks,
+    resetMe: false,
+    reset: function () {
+      if (this.resetMe) {
+        this.level = 0
+        this.returnArray = []
+        this.returnArrayChunks = []
+        this.resetMe = false
+      }
+    },
+
+    clone: function () {
+      return {
+        timeoutSate: timeoutSate,
+        level: this.level,
+        returnArray: this.returnArray.slice(0),
+        returnArrayChunks: this.returnArrayChunks.slice(0),
+        resetMe: this.resetMe,
+        reset: this.reset,
+        clone: this.clone,
+        getData: this.getData
+      }
+    },
+
+    getData: function () {
+      const me = this
+      return getFrom(0, { returnArrayChunks: me.returnArrayChunks })
+    }
+
+  }
+
+  let caller = function () {
+    // parameters
+    if (!caller.called) {
+      caller.called = true
+      return caller
+    }
+    state.reset()
+    state.level++
+    const callerArguments = Array.from(arguments)
+    if (callerArguments.length) {
+      state.returnArrayChunks.push(callerArguments)
+    }
+
+    let data = caller.data = getFrom(0, { returnArrayChunks: state.returnArrayChunks })
+
+    caller.p = () => new Promise((resolve, reject) => {
+      clearTimeout(state.timeoutSate)
+      const conedState = state.clone()
+      let ret = false
+      const data = conedState.getData()
+      if (typeof callback === 'function') {
+        ret = callback(RETURN_FROM_PROMISE, data)
+      } else {
+        ret = data
+      }
+      state.resetMe = true
+
+      return resolve(ret)
+    })
+    /* istanbul ignore else */
+    if (!arguments.length) {
+      /* istanbul ignore else */
+      if (callback) {
+        /* istanbul ignore else */
+        if (typeof callback === 'function') {
+          clearTimeout(state.timeoutSate)
+          state.resetMe = true
+
+          return callback(RETURN_FROM_CALLBACK, data)
+        }
+      }
+      /* istanbul ignore else */
+      if (!callback) {
+        return data
+      }
+    }
+    /* istanbul ignore else */
+    if (arguments.length) {
+      /* istanbul ignore else */
+      if (state.timeoutSate) {
+        clearTimeout(state.timeoutSate)
+      }
+      state.timeoutSate = safetyExecutor(data, callback)
+    }
+    state.level++
+
+    return caller
+  }
+
+  return caller(state.returnArray)
+}
+
+// exports.unlimitedCurryFactoryGetter = (paramters = false) => exports
