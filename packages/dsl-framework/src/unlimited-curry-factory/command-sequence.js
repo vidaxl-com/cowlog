229d32f1c578c2b6d2089ce1fdb8ac6e114e78eb
Imre Toth <i.toth@vidaxl.com>
2018-11-29 16:19:19 +0100

    Better documentation.
    Added array-dsl to the greenkeper configuration.

--- packages/dsl-framework/src/unlimited-curry-factory/command-sequence.js
+++ packages/dsl-framework/src/unlimited-curry-factory/command-sequence.js
@@ -1,7 +1,7 @@
 const commandParserFactory = (value) => (getProcess) => require('./get-command-arguments/commandParser')(value, getProcess)
-
 module.exports = exports = (returnObject) => function * () {
   let commands = returnObject.data.returnArrayChunks
+
   for (let i = 0; i <= commands.length - 1; i++) {
     const value = commands[i]
     const command = commands[i][0]
