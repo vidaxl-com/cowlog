b4597eef7b9fb2d66f504f0e2e179415621eec11
Imre Toth <i.toth@vidaxl.com>
2018-11-29 16:19:19 +0100

    Better documentations.

--- packages/dsl-framework/examples/array-dsl/src/index.js
+++ packages/dsl-framework/examples/array-dsl/src/index.js
@@ -1,12 +1,14 @@
 const dslF = require('dsl-framework')
 const isArray = require('isarray')
 const commandFeed = require('./commandInterpreter')
-
 module.exports = array => dslF(function (returnCode, data) {
   if (!isArray(array)) return array
   let result = array.slice(0)
+
+  for (const command of data.commandSequence()) {
+    result = commandFeed(command.command, command.arguments, result)
+  }
   data.data.returnArrayChunks.forEach((command) => {
-    result = commandFeed(command[0], command.slice(1), result)
   })
   return result
 })
