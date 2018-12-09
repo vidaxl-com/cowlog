4f44d7592e34971ac4434958cbcfef78c36c6e1c
Imre Toth <i.toth@vidaxl.com>
2018-12-09 22:12:21 +0100

    Fixed a security alert

--- packages/cowlog/src/index.js
+++ packages/cowlog/src/index.js
@@ -1,11 +1,10 @@
 'use strict'
-const merge = require('merge')
 const configParser = require('./app/configParser/configParser')
 
 module.exports = exports = function (defaultParametersToken) {
   defaultParametersToken = defaultParametersToken || 'default'
   const appContainer =
-        require('./app/container')(merge.recursive(true, configParser('default'), configParser(defaultParametersToken)))
+        require('./app/container')(Object.assign(configParser('default'), configParser(defaultParametersToken)))
 
   return appContainer.cowlog()
 }
