11d7c632329ccf0088656307a205dee8f5bcdf90
Imre Toth <i.toth@vidaxl.com>
2019-03-05 00:37:46 +0100

    [parenter] style regression fixed.

--- packages/dsl-framework/examples/parenting/src/index.js
+++ packages/dsl-framework/examples/parenting/src/index.js
@@ -1,22 +1,32 @@
 const dslF = require('dsl-framework').noTriggerEndOfExecution.noPromoises()
-  , flat = require('flat'), op = require('object-path')
-  , arrayDsl = require('array-dsl')
-  , partentFunctionFactory = (object, path) => () => op.get(object, path)
-  , childrenFunctionFactory = (object, path) => () => Object.keys(op.get(object, path)).map(key => path?
-    op.get(object, `${path}.${key}`): op.get(object, `${key}`))
+
+const flat = require('flat'); const op = require('object-path')
+
+const arrayDsl = require('array-dsl')
+
+const partentFunctionFactory = (object, path) => () => op.get(object, path)
+
+const childrenFunctionFactory = (object, path) => () => Object.keys(op.get(object, path)).map(key => path
+  ? op.get(object, `${path}.${key}`) : op.get(object, `${key}`))
 
 module.exports = (object) => dslF(function (returnCode, data) {
   const parentName = data.arguments('parentName', 'lastArgument', 'parent')
-    , childrenName = data.arguments('childrenName', 'lastArgument', 'children')
-    , noParent = data.command.has('noParent')
-    // todo: even use flat library... that will be part of the system.
-    // , noChildren = data.action.has.no.children()
 
-    //make a data abstraction system
-    //
-    , noChildren = data.command.has('noChildren'), keysPaths = Object.keys(flat(object))
-    // , registrationPath = data.command.has('toTag')?(()=>{})():data.arguments('toTag')
-    , result = arrayDsl(keysPaths.map(path => {
+  const childrenName = data.arguments('childrenName', 'lastArgument', 'children')
+
+  const noParent = data.command.has('noParent')
+
+  // todo: even use flat library... that will be part of the system.
+  // , noChildren = data.action.has.no.children()
+
+  // make a data abstraction system
+  //
+
+  const noChildren = data.command.has('noChildren'); const keysPaths = Object.keys(flat(object))
+
+  // , registrationPath = data.command.has('toTag')?(()=>{})():data.arguments('toTag')
+
+  const result = arrayDsl(keysPaths.map(path => {
     let pathArray = path.split('.')
     pathArray.pop()
     return pathArray
@@ -29,12 +39,13 @@ module.exports = (object) => dslF(function (returnCode, data) {
       aggregatedPath = idx ? aggregatedPath + '.' : ''
       aggregatedPath += pathPiece
       childrenFunctionFactory(object, parentPath)
-      !noChildren?op.get(object, aggregatedPath)[childrenName] = childrenFunctionFactory(object, parentPath):null
-      !noParent?(()=>{
-          op.get(object, aggregatedPath)[parentName] = partentFunctionFactory(object, parentPath)
-          const parentFunction = op.get(object, aggregatedPath + `.${parentName}`)
-          parentFunction.path = aggregatedPath
-        })():null
+      if (!noChildren)op.set(object, aggregatedPath + '.' + childrenName, childrenFunctionFactory(object, parentPath))
+      if (!noParent) {
+        (() => {
+          op.set(object, aggregatedPath + '.' + parentName, partentFunctionFactory(object, parentPath))
+          op.set(object, aggregatedPath + '.' + parentPath + '.path', aggregatedPath)
+        })()
+      }
     })
   })
   return object
