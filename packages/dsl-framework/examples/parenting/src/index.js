44ac472dd2cbbf8cbd7fc81af1830e66f5a2f218
Imre Toth <i.toth@vidaxl.com>
2019-03-05 00:07:23 +0100

    .noParent and .noCildren calls added.

--- packages/dsl-framework/examples/parenting/src/index.js
+++ packages/dsl-framework/examples/parenting/src/index.js
@@ -1,24 +1,26 @@
 const dslF = require('dsl-framework').noTriggerEndOfExecution.noPromoises()
-const flat = require('flat')
-const op = require('object-path')
-const arrayDsl = require('array-dsl')
+  , flat = require('flat'), op = require('object-path')
+  , arrayDsl = require('array-dsl')
+  , partentFunctionFactory = (object, path) => () => op.get(object, path)
+  , childrenFunctionFactory = (object, path) => () => Object.keys(op.get(object, path)).map(key => path?
+    op.get(object, `${path}.${key}`): op.get(object, `${key}`))
+
 module.exports = (object) => dslF(function (returnCode, data) {
-  const keysPaths = Object.keys(flat(object))
-  const result = arrayDsl(keysPaths.map(path => {
+  const parentName = data.arguments('parentName', 'lastArgument', 'parent')
+    , childrenName = data.arguments('childrenName', 'lastArgument', 'children')
+    , noParent = data.command.has('noParent')
+    // todo: even use flat library... that will be part of the system.
+    // , noChildren = data.action.has.no.children()
+
+    //make a data abstraction system
+    //
+    , noChildren = data.command.has('noChildren'), keysPaths = Object.keys(flat(object))
+    // , registrationPath = data.command.has('toTag')?(()=>{})():data.arguments('toTag')
+    , result = arrayDsl(keysPaths.map(path => {
     let pathArray = path.split('.')
     pathArray.pop()
     return pathArray
   })).unique()
-
-  const partentFunctionFactory = (object, path) => () => op.get(object, path)
-
-  const childrenFunctionFactory = (object, path) => () => {
-    return Object.keys(op.get(object, path)).map(key => {
-      return path
-        ? op.get(object, `${path}.${key}`)
-        : op.get(object, `${key}`)
-    })
-  }
 
   result.forEach(pathArray => {
     let aggregatedPath = ''
@@ -27,15 +29,12 @@ module.exports = (object) => dslF(function (returnCode, data) {
       aggregatedPath = idx ? aggregatedPath + '.' : ''
       aggregatedPath += pathPiece
       childrenFunctionFactory(object, parentPath)
-
-      // children
-      op.get(object, aggregatedPath).children = childrenFunctionFactory(object, parentPath)
-
-      // parent
-      op.get(object, aggregatedPath).parent = partentFunctionFactory(object, parentPath)
-
-      const parentFunction = op.get(object, aggregatedPath + '.parent')
-      parentFunction.path = aggregatedPath
+      !noChildren?op.get(object, aggregatedPath)[childrenName] = childrenFunctionFactory(object, parentPath):null
+      !noParent?(()=>{
+          op.get(object, aggregatedPath)[parentName] = partentFunctionFactory(object, parentPath)
+          const parentFunction = op.get(object, aggregatedPath + `.${parentName}`)
+          parentFunction.path = aggregatedPath
+        })():null
     })
   })
   return object
--- packages/dsl-framework/examples/parenting/tests/tests/test-spec.js
+++ packages/dsl-framework/examples/parenting/tests/tests/test-spec.js
@@ -2,9 +2,12 @@
 require('cowlog')()
 const {expect} = require('chai')
 const partenting = require('../../src')
+const flatten = require('flat')
+const assert = require('assert')
+const _ = require('lodash')
 
 describe('Basic Test Suite', function () {
-  exampleObject = {
+  const exampleObject = {
     a:{
       b:{
         c:1,
@@ -27,12 +30,41 @@ describe('Basic Test Suite', function () {
       {a:{b:'c'}}
     ]
   }
+  const getExample = () => _.cloneDeep(exampleObject)
 
   it('checking default results', function () {
-    const parented = partenting(exampleObject)()
+    const parented = partenting(getExample())()
     expect(parented.a.b.c3.parent()).to.deep.equal(parented.a.b)
     expect(parented.a.parent()).to.deep.equal(parented)
     expect(parented.a.children()[0].parent()).to.deep.equal(parented)
   })
+
+  it('.parentName and .childrenName', function () {
+    const parented = partenting(getExample()).parentName('p').childrenName('c')()
+    expect(parented.a.b.c3.p()).to.deep.equal(parented.a.b)
+    expect(parented.a.p()).to.deep.equal(parented)
+    expect(parented.a.c()[0].p()).to.deep.equal(parented)
+  })
+
+  it('.noChildren', function () {
+    const hasParentTags = !Object.keys(flatten(
+      partenting(getExample()).noChildren()
+    )).filter(path=>path.endsWith('.children')).length
+    assert(hasParentTags)
+  })
+
+  it('.noParent', function () {
+    const hasParentTags = !Object.keys(flatten(
+      partenting(getExample()).noParent()
+    )).filter(path=>path.endsWith('.parent')).length
+    assert(hasParentTags)
+  })
+
+  it('.noParent .noChildren', function () {
+    const other = partenting(getExample()).noParent.noChildren()
+    expect(other).to.deep.equal(exampleObject)
+  })
+
+  //no praetn no children
 
 })
