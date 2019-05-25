b070f194105d38f3545d33391d84623795f95f18
Imre Toth <i.toth@vidaxl.com>
2019-05-25 14:53:49 +0200

    Fixed regression

--- packages/require-a-lot/src/logging-and-linking/index.js
+++ packages/require-a-lot/src/logging-and-linking/index.js
@@ -1,4 +1,3 @@
-require('cowlog')()
 module.exports = (parameters, infoList, results) => {
   const { msg, consoleMessage, begin, end } = require('./message-creator')(parameters, results, infoList)
   parameters.command.has('log') && console.log(require('./prepare-before-placement')(consoleMessage))
